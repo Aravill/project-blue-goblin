5c23ef2f022f8c1570a15c9a671739a67c27df63
Aravill <mozikmichal@gmail.com>
2024-11-03 16:48:29 +0100

    fixed issue of not joining multi-word item names

--- internal/command/take.command.go
+++ internal/command/take.command.go
@@ -4,11 +4,12 @@ import (
 	"blue-goblin/internal/act"
 	"blue-goblin/internal/console"
 	"blue-goblin/internal/player"
+	"strings"
 )
 
 func TakeCommand(player *player.Player, act *act.Act, params []string) {
 	var location = act.GetLocation(player.CurrentLocation)
-	var itemName = params[0]
+	var itemName = strings.Join(params, " ")
 	var i = location.RemoveItem(itemName)
 	if i == nil {
 		console.SayLine("There is no " + itemName + " here.")
