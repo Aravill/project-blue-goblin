724b0d3607e4b79256b3b2f781bfab1796ecf3a0
Aravill <mozikmichal@gmail.com>
2024-11-04 22:41:27 +0100

    made command text grey

--- internal/console/reader.go
+++ internal/console/reader.go
@@ -2,17 +2,20 @@ package console
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
 )
 
 func Listen() (string, error) {
+	fmt.Print("\033[90m")
 	var input string
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
 		return "", err
 	}
+	defer fmt.Print("\033[0m")
 	// Remove trailing spaces and return
 	return strings.TrimSpace(input), nil
 }
