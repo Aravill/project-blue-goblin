37e5e83ad1ac13c472ef6e59ccd21da3d00965d2
Aravill <mozikmichal@gmail.com>
2024-11-04 23:09:23 +0100

    fixed issue with not closing keyboard

--- internal/console/narrator.go
+++ internal/console/narrator.go
@@ -11,32 +11,23 @@ import (
 var skipDelay int32 // Atomic flag to indicate whether to skip the delay
 
 func InitInputListener() {
-	// Open the keyboard listener
-	if err := keyboard.Open(); err != nil {
-		fmt.Println("Error opening keyboard:", err)
-		return
-	}
-	// Ensure the keyboard is closed when no longer needed
-	defer keyboard.Close()
-
-	// Start a goroutine to listen for key presses
 	go func() {
+		defer keyboard.Close()
 		for {
 			_, key, err := keyboard.GetKey()
 			if err != nil {
 				continue
 			}
-			if key == keyboard.KeySpace { // Check if the spacebar was pressed
+			if key == keyboard.KeySpace {
 				atomic.StoreInt32(&skipDelay, 1)
-				break // Exit the loop after pressing spacebar
+				break
 			}
 		}
 	}()
 }
 
 func SayLine(text string) {
-	Say(text)
-	fmt.Println()
+	Say(text + "\n")
 }
 
 func Say(text string) {
@@ -44,6 +35,15 @@ func Say(text string) {
 }
 
 func PrintInCharacters(text string) {
+
+	if err := keyboard.Open(); err != nil {
+		fmt.Println("Error opening keyboard:", err)
+		return
+	}
+
+	atomic.StoreInt32(&skipDelay, 0) // Reset the skipDelay flag
+	InitInputListener()              // Start listening for input
+
 	for _, char := range text {
 		// Curly braces are used to denote key words in text
 		if char == '{' {
@@ -59,6 +59,8 @@ func PrintInCharacters(text string) {
 			time.Sleep(20 * time.Millisecond)
 		}
 	}
+
+	keyboard.Close()
 }
 
 func SetColorYellow() {
